8343c66ae7a92415ae615c127135aaee042e6fa7
Naveen Gopinathan <44674714+navs161089@users.noreply.github.com>
2020-01-13 19:24:54 +0530

    Delete create_new_keypair.py

--- create_new_keypair.py
+++
@@ -1,7 +0,0 @@
-import boto3
-ec2 = boto3.resource('ec2')
-key_name = 'naveen_test'
-new_keypair = ec2.create_key_pair(KeyName=key_name)
-with open('/home/key/naveen_test.pem', 'w') as file:
-    file.write(new_keypair.key_material)
-    print(new_keypair.key_fingerprint)
